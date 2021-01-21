cb8792854fdfa182c9cad14a95d3aaae3ad489b9
unknown <jurgeleviciute.justina@gmail.com>
2021-01-21 18:27:23 +0200

    add initial fake store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,42 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    issues: [
+      {
+        title: "button",
+        description: "fix button",
+        status: "open",
+        trashed: false,
+        id: "0",
+      },
+      {
+        title: "button",
+        description: "fix button blue",
+        status: "closed",
+        trashed: false,
+        id: "1",
+      },
+      {
+        title: "button",
+        description: "fix yellow",
+        status: "trashed",
+        trashed: false,
+        id: "2",
+      },
+      {
+        title: "button",
+        description: "fix navigation",
+        status: "closed",
+        trashed: false,
+        id: "3",
+      },
+    ],
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {},
+});
